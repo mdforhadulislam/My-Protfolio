533dc60d760437b12a61c945051363be5955f2d2
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-02-11 10:51:08 +0600

    add file

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/icon.jpg" />
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
+}
